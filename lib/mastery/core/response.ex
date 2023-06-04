19a43fbbcb36c0b88545b4c525e0606530493116
oda <ooddaa@gmail.com>
2023-06-04 14:42:47 +0200

    [p201] Reponse.new/3

--- lib/mastery/core/response.ex
+++ lib/mastery/core/response.ex
@@ -10,4 +10,19 @@ defmodule Mastery.Core.Response do
           correct: boolean(),
           timestamp: Time.t()
         }
+
+  def new(quiz, email, answer) do
+    question = quiz.current_question
+    template = question.template
+
+    %__MODULE__{
+      quiz_title: quiz.title,
+      template_name: template.name,
+      to: question.asked,
+      email: email,
+      answer: answer,
+      correct: template.checker.(question.substitutions, answer),
+      timestamp: DateTime.utc_now()
+    }
+  end
 end
