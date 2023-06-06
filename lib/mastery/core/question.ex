d754d90075d3b7dbe5852d0300dd7b64cc238244
oda <ooddaa@gmail.com>
2023-06-06 10:04:10 +0200

    [p214] question methods

--- lib/mastery/core/question.ex
+++ lib/mastery/core/question.ex
@@ -1,5 +1,6 @@
 defmodule Mastery.Core.Question do
   defstruct ~w[asked template substitutions]a
+  alias Mastery.Core.Template
 
   @type t :: %__MODULE__{
           asked: String.t(),
@@ -7,4 +8,34 @@ defmodule Mastery.Core.Question do
           # %{substitution: any}
           substitutions: Map
         }
+
+  def new(%Template{} = template) do
+    template.generators
+    |> Enum.map(&build_substitution/1)
+    |> evaluate(template)
+  end
+
+  defp build_substitution({name, choices_or_generator}) do
+    {name, choose(choices_or_generator)}
+  end
+
+  defp choose(choices) when is_list(choices), do: Enum.random(choices)
+
+  defp choose(generator) when is_function(generator) do
+    generator.()
+  end
+
+  defp compile(template, substitutions) do
+    template.compiled
+    |> Code.eval_quoted(assigns: substitutions)
+    |> elem(0)
+  end
+
+  defp evaluate(substitutions, template) do
+    %__MODULE__{
+      asked: compile(template, substitutions),
+      template: template,
+      substitutions: substitutions
+    }
+  end
 end
--- lib/mastery/core/template.ex
+++ lib/mastery/core/template.ex
@@ -10,4 +10,13 @@ defmodule Mastery.Core.Template do
           generators: Map,
           checker: Function
         }
+
+  def new(fields) do
+    raw = Keyword.fetch!(fields, :raw)
+
+    struct!(
+      __MODULE__,
+      Keyword.put(fields, :compiled, EEx.compile_string(raw))
+    )
+  end
 end
